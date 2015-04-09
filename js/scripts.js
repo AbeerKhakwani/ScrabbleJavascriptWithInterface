6d81b3f10151d3e20ae0cf607e0cf159a502fbd2
Abeer Khakwani <abeerkhakwani@hotmail.com>
2015-04-08 17:08:12 -0700

    Make all tests pass

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var Scrabble = function(letter) {
+
+    letter = letter.toLowerCase();
+    //console.log("lettr= ", letter)
+    var letterArray = letter.split("");
+
+    //console.log("letterArray= ", letterArray)
+
+    var point = {
+        a: 1,e: 1,i: 1, o: 1, u: 1, l:1,n: 1,r: 1,s: 1,t: 1,d: 2,
+        g: 2,
+        b: 3,c: 3,m: 3,p: 3,
+        f: 4,h: 4,v: 4, w: 4,y: 4,
+        k: 5,j: 8,x: 8,
+        q: 10,z: 10
+    };
+    var returnNumber = 0;
+
+    //Foreach function loops through the letterArray and 
+    letterArray.forEach(function(word, index) {
+        var number = point[word];
+
+        number = Math.floor(number);
+        console.log("number= ", number)
+
+
+        returnNumber += number;
+    });
+
+
+    return returnNumber;
+
+
+}
+
---
+++ spec/specs.js
@@ -0,0 +1,22 @@
+describe('Scrabble', function() {
+    it("checks to see if one letter a returns 1 point", function() {
+        expect(Scrabble("a")).to.equal(1);
+    });
+    it("checks to see if a 2 point letter returns 2 points", function() {
+        expect(Scrabble("b")).to.equal(3);
+    });
+   it("checks to see if a 3 point letter and a 1 point letter returns 4 points", function() {
+        expect(Scrabble("am")).to.equal(4);
+    });
+    it("checks to see if a 3 point letter and a 1 point letter returns 4 points", function() {
+        expect(Scrabble("ab")).to.equal(4);
+    });
+       it("checks to see if a 7  point word returns 7 points", function() {
+        expect(Scrabble("Abeer")).to.equal(7);
+    });
+
+
+
+
+    
+});
